c34c8bfecadf13c18c5b6efc1614810f03514288
jcaiced <jcaiced@gmail.com>
2025-07-13 14:53:06 -0700

    following Diagram

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,6 +1,38 @@
 /**
  * Author: Jose Caicedo Created on: 7/13/25 Description:
  */
-public class Monster {
+public class Monster(Integer maxHP, Integer xp, Hashmap<String, Integer> items) {
+    this.maxHP = maxHP;
+    this.xp = xp;
+    this.items = items;
+
+  public Integer getHp() {
+
+  }
+  public void setHp(Integer hp) {
+
+  }
+  public Integer getXp() {
+
+  }
+  public HashMap<String, Integer> getItems() {
+
+  }
+  public void setItems(HashMap<String, Integer> items) {
+
+  }
+  public Integer getMaxHP() {
+
+  }
+  public boolean equals(Object o) {
+
+  }
+  public int hashCode() {
+
+  }
+
+
 
 }
+
+
